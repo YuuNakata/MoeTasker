2e3898e7bcb5552cde77a608acec704ae58b4fd5
YuuNakata <86893115+YuuNakata@users.noreply.github.com>
2025-06-18 01:55:49 -0400

    addv

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -375,7 +375,7 @@ export default async function handler(req, res) {
         const sticker = messageObject.reply_to_message.sticker;
 
         if (sticker.is_animated || sticker.is_video) {
-          await sendMessage(chatId, `¡Gomen, senpai! (｡>_<｡) Por ahora mis ojitos mágicos solo pueden analizar stickers estáticos. ¡Todavía no puedo ver los que se mueven!`, 'HTML');
+          await sendMessage(chatId, `¡Gomen, senpai! (｡>_<｡) Por ahora mis ojitos mágicos solo pueden analizar stickers estáticos. ¡Todavía no puedo ver los que se mueven!`, null);
           return;
         }
 
